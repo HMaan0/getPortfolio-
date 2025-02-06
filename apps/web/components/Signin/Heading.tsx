d41a0e5fca06b8bf989feb947d463deca0190e61
Harsh-deepsingh <153255817+Harsh-deepsingh@users.noreply.github.com>
2025-02-05 22:42:08 -0700

    dark mode only

--- apps/web/components/Signin/Heading.tsx
+++ apps/web/components/Signin/Heading.tsx
@@ -1,11 +1,11 @@
 import { useRecoilValue } from "recoil";
 import { signin } from "../../store/Signin";
-
+import Image from "next/image";
 const Heading = () => {
   const newUser = useRecoilValue(signin);
   return (
     <>
-      <span className="px-4 py-1.5 rounded-full bg-green-400"></span>
+      <Image src="logo.svg" width={50} height={50} alt="get portfolio" />
       <div className="text-center flex flex-col sm:m-0 mb-10">
         <h3 className="text-xl">
           {newUser ? "Let’s get started" : "Welcome back"}
@@ -13,7 +13,7 @@ const Heading = () => {
         <div className="w-4/3 container m-auto">
           <p className="text-gray-500 text-wrap">
             {newUser
-              ? "         Get a professional portfolio website built in seconds AI and responsive design!"
+              ? "Get a professional portfolio website built in seconds and responsive design!"
               : "Please enter your details to log in."}
           </p>
         </div>
